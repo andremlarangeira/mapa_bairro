5091cff899674d5f92aae3dfec7c3ca76799897c
Andre Larangeira <andremlarangeira@gmail.com>
2017-11-21 22:45:11 -0200

    design versao 1

--- js/app.js
+++ js/app.js
@@ -1,7 +1,28 @@
 var menu = $(".menu");
 var icon = $(".menu-icon");
+var divMap = $("#map");
 
 icon.on('click', function(){
-   console.log("teste");
    menu.toggleClass("open");
 });
+
+divMap.on('click', function(){
+   menu.removeClass("open");
+})
+
+var map;
+function initMap() {
+   map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -22.738729, lng:-47.616529},
+      zoom: 16
+   });
+
+   var lobato = {lat: -22.746132, lng: -47.635882};
+
+   var marker = new google.maps.Marker({
+      position: lobato,
+      map: map,
+      title: 'casa lobato',
+      animation: google.maps.Animation.DROP
+   });
+}
